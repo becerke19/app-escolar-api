55faef954060a93de722dc2801b789fcb17fe89d
becerke19 <becerke19@gmail.com>
2025-12-05 14:00:10 -0600

    Corregir instalación de psycopg2 para Render

--- build.sh
+++ build.sh
@@ -2,8 +2,17 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
 pip install --upgrade pip
+
+# Install psycopg2 dependencies
+pip install --upgrade pip wheel setuptools
+
+# Install requirements
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
